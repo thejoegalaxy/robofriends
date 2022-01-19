cd6ff411e08f4fced906a5dbcba94e448f2b8e05
C Davis <cdavis@joegalaxy.com>
2022-01-18 20:24:23 -0600

    implemented dynamic Searchbox

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import Searchbox from './Searchbox';
+
+// parent to child properties
+// STATE >> props
 
 class App extends React.Component {
-  render() { //component must render something...
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <Searchbox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //used for websites, the dom, could be ReactNative, for mobile phones.
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
