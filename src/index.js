aa108da84b77eac59278383b25cd34931a2a09f0
C Davis <cdavis@joegalaxy.com>
2022-01-18 18:14:25 -0600

    implemented Robot cards

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //used for websites, the dom, could be ReactNative, for mobile phones.
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from './robots';
 
 ReactDOM.render(
-    <Hello greeting={'Hello React Ninja'} />, //created our own tag/component: Hello. and created a greeting property.
-  document.getElementById('root')
+  <div>
+  <Card id={robots[0].name} email={robots[0].email} name={robots[0].name} />
+  <Card id={robots[1].name} email={robots[1].email} name={robots[1].name} />
+  <Card id={robots[2].name} email={robots[2].email} name={robots[2].name} />
+  </div>
+    , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
